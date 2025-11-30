0a7691833e680083cb0ba2161b20437601ee9970
UmaMahiii <umamaheswaris9074@gmail.com>
2025-11-30 16:30:43 +0530

    Set EC2 instance_type to t3.medium for Strapi

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "strapi" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.small"
+  instance_type = "t3.medium"
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
   key_name               = "strapi-key"
 
