47eae5cabfc8380f3dbb4c3508d6d8cb7b4d95e6
UmaMahiii <umamaheswaris9074@gmail.com>
2025-11-30 16:46:11 +0530

    Simplify Strapi DB config to fixed postgres

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,53 +1,18 @@
 import path from 'path';
 
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections: any = {
-    mysql: {
-      client: 'mysql2',
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY'),
-          cert: env('DATABASE_SSL_CERT'),
-          ca: env('DATABASE_SSL_CA'),
-          capath: env('DATABASE_SSL_CAPATH'),
-          cipher: env('DATABASE_SSL_CIPHER'),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
+export default ({ env }) => ({
+  client: 'postgres',
+  connection: {
+    connectionString: env('DATABASE_URL'),
+    host: env('DATABASE_HOST', 'localhost'),
+    port: env.int('DATABASE_PORT', 5432),
+    database: env('DATABASE_NAME', 'strapi'),
+    user: env('DATABASE_USERNAME', 'strapi'),
+    password: env('DATABASE_PASSWORD', 'strapi'),
+    ssl: env.bool('DATABASE_SSL', false) && {
+      rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
     },
-    postgres: {
-      client: 'postgres',
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-    },
-    sqlite: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-    },
-  };
-
-  return {
-    client,
-    connection: connections[client].connection,
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-  };
-};
+    schema: env('DATABASE_SCHEMA', 'public'),
+  },
+  acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+});
